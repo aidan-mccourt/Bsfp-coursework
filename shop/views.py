5b408a4f6e303a118c84afe37f4e2425f460403b
aidan-mccourt <48483803+aidan-mccourt@users.noreply.github.com>
2021-11-23 18:19:36 +0000

    commit test

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 #from .models import Flavour#, Ice_cream, Topping, Milk_shake
-#import models
+
 
 def home(request):
     return render(request, 'shop/home.html')
