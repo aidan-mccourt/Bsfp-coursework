1e209423db08728f0ced38e9b116be4cbd03659c
aidan-mccourt <48483803+aidan-mccourt@users.noreply.github.com>
2021-12-07 13:49:51 +0000

    added fields to milk_shake table

--- shop/models.py
+++ shop/models.py
@@ -1,34 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from shop.views import ice_cream
+#rom shop.views import ice_cream
 
 #from shop.views import flavour, ice_cream, topping
 
 class Ice_cream(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 100, unique=True)
     cost = models.FloatField(null=True, blank=True, default=None)
     def __str__(self):
         return self.name 
 
 class Flavour(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 100, unique=True)
     cost = models.FloatField(null=True, blank=True, default=None)
 
     def __str__(self):
         return self.name 
 
 class Topping(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 100, unique=True)
     cost = models.FloatField(null=True, blank=True, default=None)
 
     def __str__(self):
         return self.name 
 
 class Milk_shake(models.Model):
-    m_ice_cream = models.ForeignKey(Ice_cream,on_delete=None)
-    m_flavour = models.ForeignKey(Flavour,on_delete=None)
-    m_topping = models.ForeignKey(Topping,on_delete=None)
+    m_ice_cream = models.ForeignKey(Ice_cream,to_field='name',on_delete=None, default='none')
+    m_flavour = models.ForeignKey(Flavour,to_field='name',on_delete=None, default='none')
+    m_topping = models.ForeignKey(Topping,to_field='name',on_delete=None, default='none')
     m_cost = models.FloatField(null=True, blank=True, default=None) 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 # Create your models here.
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from .models import Flavour#, Ice_cream, Topping, Milk_shake
+from .models import Flavour, Ice_cream, Topping, Milk_shake
 
 
 def home(request):
