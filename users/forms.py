92ff8b7a9bf9cebb261a8d0f84ebc2611b43f0e3
aidan-mccourt <48483803+aidan-mccourt@users.noreply.github.com>
2021-12-07 16:56:13 +0000

    register form added

---
+++ users/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import fields
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
+
+    class Meta:# gives nested name space and keeps configs in one space
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
